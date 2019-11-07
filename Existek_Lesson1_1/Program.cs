b114d6a312bb97f5db7732b92e18b43d69d89ce2
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-11-07 16:35:10 +0200

    Add project files.

---
+++ Existek_Lesson1_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Existek_Lesson1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 78;
+            Int32 b = 100;
+            var c = 89;
+
+            Console.WriteLine($"A = {a}");
+            Console.WriteLine($"B = {b}");
+            Console.WriteLine(c);
+
+            a = 129;
+            Console.WriteLine($"A = {a}");
+
+            float f = 56.92f;
+            double d = 129.7341774256;
+
+            Console.WriteLine($"F = {f}");
+            Console.WriteLine($"D = {d}");
+            float fg = f + 23.89f;
+
+            Console.WriteLine(fg);
+
+            Console.ReadLine();
+        }
+    }
+}
